62974b2c010f0676c1427542c299f0ed4ff5a6d4
Val2807 <valentin.nikonorov00@mail.ru>
2025-09-16 13:54:16 +0300

    added mistakes checking

--- by_semen.py
+++ by_semen.py
@@ -27,21 +27,25 @@ import math
 
 
 def quadratic_equation (*, a: float, b: float, c: float) -> str:
-      if a == 0:
-          answer = "Это не квадратное уравнение (a = 0)"
-      else:
-            D = b**2 - 4*a*c
-            if D > 0: 
-                  sqrt_D = math.sqrt(D)
-                  x1 = (-b + sqrt_D) / (2*a)
-                  x2 = (-b - sqrt_D) / (2*a)
-                  answer = f"Два корня: {x1:.1f} и {x2:.1f}"
-            elif D == 0:
-                  x = -b / (2*a)
-                  answer = f"Один корень: {x:.1f}"
+      try:
+            if a == 0:
+                  answer = "Это не квадратное уравнение (a = 0)"
+            
             else:
-                  answer = "Корней нет (дискриминант меньше 0)"
-      return answer 
+                  D = b**2 - 4*a*c
+                  if D > 0: 
+                        sqrt_D = math.sqrt(D)
+                        x1 = (-b + sqrt_D) / (2*a)
+                        x2 = (-b - sqrt_D) / (2*a)
+                        answer = f"Два корня: {x1:.1f} и {x2:.1f}"
+                  elif D == 0:
+                        x = -b / (2*a)
+                        answer = f"Один корень: {x:.1f}"
+                  else:
+                        answer = "Корней нет (дискриминант меньше 0)"
+            return answer 
+      except Exception as e:
+            return f"Ошибка: {e}"
 
 print(quadratic_equation(a=8, b=69, c=-21))
 
